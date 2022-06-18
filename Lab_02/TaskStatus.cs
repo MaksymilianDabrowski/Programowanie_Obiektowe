2430f388350bba05ddc0fc027f6c02f7bf995a82
MaksymilianDabrowski <107003776+MaksymilianDabrowski@users.noreply.github.com>
2022-06-18 17:05:18 +0200

    Add files via upload

---
+++ Lab_02/TaskStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace lab_2
+{
+    enum TaskStatus
+    {
+        // Status w wartości enum 
+        Waiting,
+        Progress,
+        Done,
+        Rejected
+    };
+}
---
+++ Lab_02/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2
+{
+    internal class Teacher : Person
+    {
+        // Klasa Teacher z dziedziczenia Person
+        public Teacher(string name, int age) : base(name, age)
+        {
+            this.name = name ?? throw new ArgumentNullException(nameof(name));
+            this.age = age;
+        }
+
+        // Wypis nauczyciela 
+        public override string ToString()
+        {
+            return $"Treacher: {name} ({age} y.o.)";
+        }
+    }
+}
